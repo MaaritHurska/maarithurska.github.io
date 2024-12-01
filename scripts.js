123da4a4fecf3eb3df9d98c16967b41a1f95e38b
MaaritHurska <163125395+MaaritHurska@users.noreply.github.com>
2024-12-01 20:44:05 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const text = "Tervetuloa sivuilleni!";
-    const animatedText = document.getElementById('animated-text');
-    let index = 0;
+//document.addEventListener('DOMContentLoaded', () => {
+    //const text = "Tervetuloa sivuilleni!";
+    //const animatedText = document.getElementById('animated-text');
+    //let index = 0;
 
-    function type() {
-        if (index < text.length) {
-            animatedText.textContent += text.charAt(index);
-            index++;
-            setTimeout(type, 100); // Adjust the typing speed (milliseconds)
-        }
-    }
+    //function type() {
+        //if (index < text.length) {
+            //animatedText.textContent += text.charAt(index);
+            //index++;
+            //setTimeout(type, 100); // Adjust the typing speed (milliseconds)
+        //}
+    //}
 
     // Start typing animation
-    type();
+    //type();
 });
