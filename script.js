38bd0145e8552c7c6b5a8c36c3ec68195cc925f9
MaaritHurska <163125395+MaaritHurska@users.noreply.github.com>
2025-02-12 11:04:10 +0200

    Update script.js
    piilotus mobiili

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const text = "Tervetuloa sivuilleni!";
     const animatedText = document.getElementById('animated-text');
-    let index = 0;
-
-    function type() {
-        if (index < text.length) {
-            animatedText.textContent += text.charAt(index);
-            index++;
-            setTimeout(type, 100); // Adjust the typing speed (milliseconds)
-        }
-    }
 
     // Tarkistetaan näytön leveys ennen animaation käynnistämistä
     if (window.innerWidth >= 768) {
+        let index = 0;
+
+        function type() {
+            if (index < text.length) {
+                animatedText.textContent += text.charAt(index);
+                index++;
+                setTimeout(type, 100); // Adjust the typing speed (milliseconds)
+            }
+        }
+
         type(); // Käynnistetään animaatio vain, jos näyttö on tarpeeksi suuri
     } else {
-        animatedText.textContent = text; // Mobiilissa näytetään vain valmis teksti
+        animatedText.style.display = "none"; // Piilotetaan teksti mobiilissa
     }
 });
